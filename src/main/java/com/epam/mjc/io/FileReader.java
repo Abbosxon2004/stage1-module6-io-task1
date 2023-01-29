0a07ac80d103e68d96c0218e48948119367ed265
abboskhan <abbosxon28012004@gmail.com>
2023-01-29 18:03:52 +0500

    last

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -12,13 +12,12 @@ public class FileReader {
         Profile profile = new Profile();
         try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
             String str = "";
-
             while ((str = reader.readLine()) != null) {
                 int index = str.indexOf(":");
-                if (str.contains("Name")) profile.setName(str.substring(index+2));
-                if (str.contains("Age")) profile.setAge(Integer.parseInt(str.substring(index+2)));
-                if (str.contains("Email")) profile.setEmail(str.substring(index+2));
-                if (str.contains("Phone")) profile.setPhone(Long.valueOf(str.substring(index+2)));
+                if (str.contains("Name")) profile.setName(str.substring(index + 2));
+                else if (str.contains("Age")) profile.setAge(Integer.parseInt(str.substring(index + 2)));
+                else if (str.contains("Email")) profile.setEmail(str.substring(index + 2));
+                else if (str.contains("Phone")) profile.setPhone(Long.valueOf(str.substring(index + 2)));
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
