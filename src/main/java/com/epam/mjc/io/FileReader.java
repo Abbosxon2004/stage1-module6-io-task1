046d52fdc4bb729ac239b7760a8cd9aeeeba43c1
abboskhan <abbosxon28012004@gmail.com>
2023-01-29 17:57:47 +0500

    all

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -1,11 +1,28 @@
 package com.epam.mjc.io;
 
-import java.io.File;
+import java.io.*;
 
 
 public class FileReader {
+    public static void main(String[] args) {
 
-    public Profile getDataFromFile(File file) {
-        return new Profile();
+    }
+
+    public static Profile getDataFromFile(File file) {
+        Profile profile = new Profile();
+        try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
+            String str = "";
+
+            while ((str = reader.readLine()) != null) {
+                int index = str.indexOf(":");
+                if (str.contains("Name")) profile.setName(str.substring(index+2));
+                if (str.contains("Age")) profile.setAge(Integer.parseInt(str.substring(index+2)));
+                if (str.contains("Email")) profile.setEmail(str.substring(index+2));
+                if (str.contains("Phone")) profile.setPhone(Long.valueOf(str.substring(index+2)));
+            }
+            return profile;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
