0c13f01eba5aa66812d42e69fa3ff2011e11f869
abboskhan <abbosxon28012004@gmail.com>
2023-01-29 18:07:00 +0500

    last

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -4,23 +4,23 @@ import java.io.*;
 
 
 public class FileReader {
-    public static void main(String[] args) {
-
-    }
-
     public static Profile getDataFromFile(File file) {
         Profile profile = new Profile();
         try (BufferedReader reader = new BufferedReader(new java.io.FileReader(file))) {
-            String str = "";
+            String str;
             while ((str = reader.readLine()) != null) {
                 int index = str.indexOf(":");
-                if (str.contains("Name")) profile.setName(str.substring(index + 2));
-                else if (str.contains("Age")) profile.setAge(Integer.parseInt(str.substring(index + 2)));
-                else if (str.contains("Email")) profile.setEmail(str.substring(index + 2));
-                else if (str.contains("Phone")) profile.setPhone(Long.valueOf(str.substring(index + 2)));
+                if (str.contains("Name"))
+                    profile.setName(str.substring(index + 2));
+                else if (str.contains("Age"))
+                    profile.setAge(Integer.parseInt(str.substring(index + 2)));
+                else if (str.contains("Email"))
+                    profile.setEmail(str.substring(index + 2));
+                else if (str.contains("Phone"))
+                    profile.setPhone(Long.parseLong(str.substring(index + 2)));
             }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+        } catch (IOException | NumberFormatException e) {
+            e.printStackTrace();
         }
         return new Profile(profile.getName(), profile.getAge(), profile.getEmail(), profile.getPhone());
     }
